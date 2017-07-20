18efd110fc6a73f9761148a1ca92cb1c0802cea0
xupeiyuan <xupeiyuan@zhuanzhuan.com>
2017-07-20 16:22:14 +0800

    一键入库逻辑

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const storage = [
+  {
+    id: 1,
+    name: '库位1',
+    size: 2,
+    store: []
+  },
+  {
+    id: 2,
+    name: '库位2',
+    size: 20,
+    store: []
+  },
+  {
+    id: 3,
+    name: '库位3',
+    size: 10,
+    store: []
+  }
+]
